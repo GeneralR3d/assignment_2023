ffb880a74aacc880d11b8650c8f8c4e9795b3ae8
TdrDingren <97043423+TdrDingren@users.noreply.github.com>
2023-06-13 10:58:24 +0800

    Added logic

--- rpc-server/main.go
+++ rpc-server/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	rpc "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc/imservice"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
+	"log"
 )
 
 func main() {
+
+	err := rdb.InitClient(ctx, "redis:6379", "")
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to init Redis client, err: %v", err)
+		log.Fatal(errMsg)
+	}
+
 	r, err := etcd.NewEtcdRegistry([]string{"etcd:2379"}) // r should not be reused.
 	if err != nil {
 		log.Fatal(err)
